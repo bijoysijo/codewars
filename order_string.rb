9c8bff465dc005d272a24209fb03b0ae103bae11
Bijoy Sijo <bijoysijo21@gmail.com>
2021-07-11 07:08:57 +0530

    6 Kyu Order String - Sort by

---
+++ order_string.rb
@@ -0,0 +1,17 @@
+# def order(words)
+#   word_array = words.split(/ /)
+#   index_array = word_array.map {|x| x[/\d+/]}
+#   beautiful_hash = {}
+
+#   word_array.each_with_index do |e, i|
+#     beautiful_hash[e] = index_array[i]
+#   end
+
+#   beautiful_hash.keys.sort { |a, b| beautiful_hash[a] <=> beautiful_hash[b] }.join(' ')
+# end
+
+def order(words)
+  words.split.sort_by{ |w| w[/\d/] }.join(' ')
+end
+
+p order("is2 Thi1s T4est 3a")
